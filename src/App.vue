0f7a5e236398f6bb96dd65795ab134425b7c7294
李继龙 <kljzndx@outlook.com>
2024-09-08 16:00:29 +0800

    添加输入区按键

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
+import Button from '@/components/Button.vue';
+
+const inputBtn = "123456789*0#";
 </script>
 
 <template>
-  
+  <div>
+    <ul class="w-48 flex flex-wrap">
+        <li v-for="str of inputBtn" :key="str">
+            <Button :text="str" @pointer-down="console.log(`key-down: ${str}`)" @pointer-up="console.log(`key-up: ${str}`)"></Button>
+        </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
