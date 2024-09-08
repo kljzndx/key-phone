70da854f81df70624c8ff1fb8612e8254b10206d
李继龙 <kljzndx@outlook.com>
2024-09-08 15:59:16 +0800

    添加按钮组件

---
+++ src/components/Button.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+    text: string
+}>(), {
+    text: ''
+});
+
+const em = defineEmits<{
+    pointerDown: [text: string]
+    pointerUp: [text: string]
+}>();
+</script>
+
+<template>
+    <a class="w-16 h-8 block text-xl text-center bg-orange-300 border-2 border-orange-900" @click.prevent
+        @pointerdown.stop.prevent="em('pointerDown', props.text)" @pointerup.stop.prevent="em('pointerUp', props.text)">
+        <slot name="content">{{ props.text }}</slot>
+    </a>
+</template>
+
+<style>
+
+</style>
