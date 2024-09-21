42f8bff20249df06043aa606e0bc56f3415ff9a3
李继龙 <kljzndx@outlook.com>
2024-09-21 19:01:17 +0800

    优化404页面显示

--- src/screens/PageNotFound.vue
+++ src/screens/PageNotFound.vue
@@ -3,8 +3,8 @@ import Container from './container.vue';
 </script>
 <template>
     <Container rt-fn-text="返回" rt-fn-url="#back">
-        <div class=" w-full h-full text-white text-2xl flex justify-center items-center">
-            示例页面
+        <div class=" w-full h-full text-white text-xl flex justify-center items-center">
+            此功能尚未做好
         </div>
     </Container>
 </template>
